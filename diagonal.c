ba21058f31c49e6c34ee8109d7c021f717352f23
JMJ Riyana <jeemariyana@gmail.com>
2020-02-11 19:32:19 +0530

    init

---
+++ diagonal.c
@@ -0,0 +1,57 @@
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+int n,ar[100][100],i,j,sum1=0,sum2=0;
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        scanf("%d",&ar[i][j]);
+    }
+}
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        if(i==j)
+        {
+            sum1=sum1+ar[i][j];
+        }
+    }
+}
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        if(j==(n-(i+1)))
+        {
+            sum2=sum2+ar[i][j];
+        }
+    }
+}
+int diff=sum1-sum2;
+
+if(diff<0)
+{
+    printf("%d",diff*(-1));
+}
+else 
+{
+    printf("%d",diff);
+}
+
+
+
+return 0;
+}
